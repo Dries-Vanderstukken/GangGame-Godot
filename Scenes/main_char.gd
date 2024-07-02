865d9db64de44eed379481adc2fc2a632f89f4be
Haii <160888727+Dries-Vanderstukken@users.noreply.github.com>
2024-07-02 13:36:28 +0200

    Basic
    Movement/ collision
    Bigger scene
    Coyote timings

--- Scenes/main_char.gd
+++ Scenes/main_char.gd
@@ -1,28 +1,44 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 500.0
+const JUMP_VELOCITY = -1200.0
+@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var coyote_timer = $CoyoteTimer
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
+	# Animations
+	if (velocity.x > 1 || velocity.x < -1):
+		animated_sprite_2d.animation = "Walk"
+	else:
+		animated_sprite_2d.animation = "Idle"
+		
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		animated_sprite_2d.animation = "Jump"
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and (is_on_floor() || !coyote_timer.is_stopped()):
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("Left", "Right")
 	if direction:
 		velocity.x = direction * SPEED
+		if direction > 0:
+			animated_sprite_2d.flip_h = 0
+		else:
+			animated_sprite_2d.flip_h = 1
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, 50)
 
+	var was_on_floor = is_on_floor()
 	move_and_slide()
+	if (was_on_floor == !is_on_floor()):
+		coyote_timer.start()
